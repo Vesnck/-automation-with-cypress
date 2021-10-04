5abe6b06a5d7b7e7dc05b4662e51994fc11fb3cf
Ekaterina Vesnina <vesnina.katya17@gmail.com>
2021-10-04 23:05:32 +0300

    first commit

---
+++ cypress/integration/examples/test1.js
@@ -0,0 +1,9 @@
+describe("My first test suite", function() {
+it("My first test case", function(){
+    cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/")
+})
+it("My second test case", function(){
+    ///
+})
+
+})
