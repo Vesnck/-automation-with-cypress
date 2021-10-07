525a7264e2291c942eecd56f1fb4b77439602af0
Ekaterina Vesnina <vesnina.katya17@gmail.com>
2021-10-07 23:24:35 +0300

    chapter 1 completed

--- cypress/integration/examples/test1.js
+++ cypress/integration/examples/test1.js
@@ -1,6 +1,11 @@
+/// <reference types = "Cypress" />
+
 describe("My first test suite", function() {
 it("My first test case", function(){
     cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/")
+    cy.get('.search-keyword').type('ca')
+    cy.wait(1000)
+    cy.get('.product:visible').should('have.length',4)
 })
 it("My second test case", function(){
     ///
