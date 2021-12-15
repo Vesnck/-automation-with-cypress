91ed47d5848b598d578818b9f7ec5fbb7909d464
Ekaterina Vesnina <vesnina.katya17@gmail.com>
2021-12-15 22:35:46 +0100

    cucumber dataTable and reporting

--- cypress/integration/examples/BDD/e-commerce.feature
+++ cypress/integration/examples/BDD/e-commerce.feature
@@ -2,8 +2,18 @@ Feature: End-to-end e-commerce validation
 
 
     Application regression
+    @Regression
     Scenario: E-commerce product delivery
     Given I open e-commerce page
     When I add items to cart
     And Validate the total prices and checkout
     Then select the country, submit and verify success 
+
+    @Smoke
+    Scenario: Filling the order form
+    Given I open e-commerce page
+    When I fill the form details
+    |name |gender|
+    |frank| Male |
+    Then Form behaviour is changed
+    And Navigate to Shop page
--- cypress/integration/examples/BDD/e-commerce/e-comStepDef.js
+++ cypress/integration/examples/BDD/e-commerce/e-comStepDef.js
@@ -4,10 +4,21 @@ import ProductPage from '../../../../support/pageObjects/ProductPage'
 import CheckoutPage from '../../../../support/pageObjects/CheckoutPage'
 import DeliveryPage from '../../../../support/pageObjects/DeliveryPage'
 
+//html cucumber report
+//> node cucumber-html-reporter.js   
+ 
+//run cucumber feature
+//> node_modules/.bin/cypress run --spec cypress\integration\examples\BDD\e-commerce.feature --headed --browser chrome
+
+//run tagged scenaros
+//> npx cypress-tags run -e TAGS="@Smoke" --headed --browser chrome
+
+
 const productPage = new ProductPage()
 const homePage = new HomePage()
 const checkoutPage = new CheckoutPage()
 const deliveryPage = new DeliveryPage()
+let name
 
 
 Given("I open e-commerce page", () => {
@@ -50,4 +61,23 @@ Then("select the country, submit and verify success", () => {
         const messageText = element.text()
         expect(messageText.includes('Success! Thank you!')).to.be.true
     })
+})
+
+When("I fill the form details", function(dataTable){
+
+    name = dataTable.rawTable[1][0]
+    homePage.getEditBox().type(name)
+    homePage.getGender().select(dataTable.rawTable[1][1])
+})
+
+Then("Form behaviour is changed", function(dataTable){
+    homePage.getTwoWayDataBinding().should('have.value',name)
+    homePage.getEditBox().should('have.attr', 'minlength', '2')
+    homePage.getEntrepreneur().should('be.disabled')
+    homePage.getEmployed().should('not.be.disabled')
+
+})
+
+And("Navigate to Shop page", function(){
+    homePage.getShopTab().click()
 })
